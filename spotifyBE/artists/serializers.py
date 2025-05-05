e7d44a6ff57eeb54059f16e31839a0fca06e956f
trancuong19022 <trancuongtn82@gmail.com>
2025-05-05 18:49:45 +0700

    add

--- spotifyBE/artists/serializers.py
+++ spotifyBE/artists/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from spotifyBE.artists.models import Artists
 from spotifyBE.albums.models import Albums
+from spotifyBE.relationships.serializers import ArtistTracksSerializer
 
 class ArtistsSerializer(serializers.ModelSerializer):
     # albums = SimpleAlbumsSerializer(many=True, read_only=True)
+   
     class Meta:
         model = Artists
         fields = '__all__'
