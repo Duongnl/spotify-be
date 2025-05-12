3069a78b908228109b33795382a1c223413d41bf
LocHoang1 <lochoang2101@gmail.com>
2025-05-12 16:46:00 +0700

    update user

--- spotifyBE/users/urls.py
+++ spotifyBE/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, RefreshTokenView, LogoutView, UserDetailView, UserByIdView
+from .views import RegisterView, LoginView, RefreshTokenView, LogoutView, UserDetailView, UserByIdView, UpdateUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -8,4 +8,5 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('users/me/', UserDetailView.as_view(), name='user-detail'),
     path('users/<int:id>/', UserByIdView.as_view(), name='user-by-id'),
+    path('users/update/<int:id>/', UpdateUserView.as_view(), name='update-user'),
 ]
