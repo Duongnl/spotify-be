e483d926aa9fdf0b306e5d4794c0b931ed96ec78
Duongg <lengocduong003@gmail.com>
2025-05-14 00:00:51 +0700

    create playbar auto

--- spotifyBE/users/urls.py
+++ spotifyBE/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, RefreshTokenView, LogoutView, UserDetailView, UserByIdView, UpdateUserView
+from .views import RegisterView, LoginView, RefreshTokenView, LogoutView, UserDetailView, UserByIdView, UpdateUserView, UpdateUserPlaybarIdView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('users/me/', UserDetailView.as_view(), name='user-detail'),
     path('users/<int:id>/', UserByIdView.as_view(), name='user-by-id'),
     path('users/update/<int:id>/', UpdateUserView.as_view(), name='update-user'),
+    path('users/update-playbar/<int:id>/', UpdateUserPlaybarIdView.as_view(), name='update-user-playbar'),
 ]
